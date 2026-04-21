4ef22fcf6055f44dfd94f22ac52a6d40faa37d99
Sudhanshu <sudhanshumani94@gmail.com>
2026-04-21 20:18:01 +0530

    cleanup before subtree add

--- server/src/api/fetchSP500.ts
+++
@@ -1,9 +0,0 @@
-// src/api/fetchSP500.ts
-import { fetchPrices, PriceDay } from './fetchPrice.js'
-
-export async function fetchSP500(
-  startDate: string,
-  endDate:   string
-): Promise<PriceDay[]> {
-  return fetchPrices('SPY', startDate, endDate)
-}
